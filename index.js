a970a3326d9b2c67e815c28bf42c7d552fedd00e
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-09-17 23:31:25 -0700

    Added interval between POST calls

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = express();
 var request = require('request').defaults({
     jar: true
 });
+var sleep = require('sleep');
 
+var D = false;
 
 app.get('/', function(req, res) {
     var url = 'https://www.studentuniverse.com/wapi/flightsWapi/searchFlightsSpanned';
@@ -25,17 +27,31 @@ app.get('/', function(req, res) {
         "phoneInDiscountCode": null,
         "searchKey": null
     }
+    var table = {},
+        counter = 1;
+    var sources = ['LAX', 'CHI', 'PHX', 'SFO', 'PDX', 'DFW', 'IAH', 'NYC', 'BOS'],
+        destinations = ['BOM', 'HYD', 'DEL', 'MAA', 'BLR'];
+    var startDates = ['2015-12-25T00:00:00.000', '2015-12-26T00:00:00.000'],
+        endDates = ['2016-01-17T00:00:00.000', '2016-01-18T00:00:00.000', '2016-01-19T00:00:00.000'];
 
-    var table = {};
-    var sources = ['LAX', 'CHI'],
-        destinations = ['BOM', 'HYD'];
-    var startDates = ['2015-12-16T00:00:00.000', '2015-12-17T00:00:00.000'],
-        endDates = ['2016-01-10T00:00:00.000', '2016-01-11T00:00:00.000'];
+    // var sources = ['LAX', 'CHI', 'PHX'],
+    //     destinations = ['BOM', 'HYD', 'DEL', 'MAA', 'BLR'];
+    // var startDates = ['2015-12-16T00:00:00.000', '2015-12-17T00:00:00.000', ],
+    //     endDates = ['2016-01-03T00:00:00.000', '2016-01-04T00:00:00.000', '2016-01-05T00:00:00.000'];
+
+    var total = sources.length * destinations.length * startDates.length * endDates.length;
+    var debug_count = total / 10; // for every 10% percent print debug string
+
+    console.log('Cities,Start,Date,End Date,Price');
+
     for (var isource = 0; isource < sources.length; isource++) {
         for (var idestination = 0; idestination < destinations.length; idestination++) {
             table[sources[isource] + "-" + destinations[idestination]] = [];
             for (var isd = 0; isd < startDates.length; isd++) {
                 for (var ied = 0; ied < endDates.length; ied++) {
+
+                    sleep.usleep(500000); // 500 milli seconds
+
                     data.tripElements[0].dateTime = startDates[isd]; // set start date
                     data.tripElements[0].origin = sources[isource]; // set origin for onward
                     data.tripElements[0].destination = destinations[idestination]; // set destination for onward
@@ -44,6 +60,11 @@ app.get('/', function(req, res) {
                     data.tripElements[1].origin = destinations[idestination]; // set origin for return
                     data.tripElements[1].destination = sources[isource]; // set destination for return
 
+                    if (D && total % debug_count === 0) {
+                        console.log('Sent req: ', data.tripElements);
+                    };
+                    total -= 1;
+
                     request({
                         method: 'POST',
                         uri: url,
@@ -51,8 +72,8 @@ app.get('/', function(req, res) {
                         json: true
                     }, function(error, response, body) {
                         if (error) {
-                            console.log('error');
-                            res.status(500).send("Error in POST call");
+                            console.log(counter, 'error', error);
+                            //res.status(500).send("Error in POST call");
                         } else {
                             var cheapest_itinerary = {
                                 sd: null,
@@ -61,30 +82,33 @@ app.get('/', function(req, res) {
                             };
                             var source, destination;
                             if (!body.itineraries) {
-                                console.log('Couldnt find itineraries for a combination');
-                                return;
-                            };
-                            for (var i = 0; i < body.itineraries.length; i++) {
-                                var itinerary = body.itineraries[i];
-                                if (itinerary.total < cheapest_itinerary.price) {
-                                    cheapest_itinerary.sd = itinerary.legs[0].departureTime;
-                                    cheapest_itinerary.ed = itinerary.legs[itinerary.legs.length - 1].departureTime;
-                                    cheapest_itinerary.price = itinerary.total;
+                                if (D) {
+                                    console.log(counter, 'Couldnt find itineraries for a combination');
+                                };
+                            } else {
+                                for (var i = 0; i < body.itineraries.length; i++) {
+                                    var itinerary = body.itineraries[i];
+                                    if (itinerary.total < cheapest_itinerary.price) {
+                                        cheapest_itinerary.sd = itinerary.legs[0].departureTime;
+                                        cheapest_itinerary.ed = itinerary.legs[itinerary.legs.length - 1].departureTime;
+                                        cheapest_itinerary.price = itinerary.total;
 
-                                    source = itinerary.legs[0].departureAirport;
-                                    destination = itinerary.legs[0].arrivalAirport;
+                                        source = itinerary.legs[0].departureAirport;
+                                        destination = itinerary.legs[0].arrivalAirport;
+                                    }
                                 }
+                                // console.log(source + '-' + destination);
+                                if (!table[source + '-' + destination]) {
+                                    table[source + '-' + destination] = [];
+                                };
+                                table[source + '-' + destination].push(cheapest_itinerary);
+                                // console.log('table', table);
+                                // console.log(source + '-' + destination, JSON.stringify(cheapest_itinerary));
+                                console.log([counter, source + '-' + destination, cheapest_itinerary.sd, cheapest_itinerary.ed, cheapest_itinerary.price].join(','));
                             }
-                            // console.log(source + '-' + destination);
-                            if (!table[source + '-' + destination]) {
-                                table[source + '-' + destination] = [];
-                            };
-                            table[source + '-' + destination].push(cheapest_itinerary);
-                            // console.log('table', table);
-                            // console.log(source + '-' + destination, JSON.stringify(cheapest_itinerary));
-                            console.log([source + '-' + destination, cheapest_itinerary.sd, cheapest_itinerary.ed, cheapest_itinerary.price].join(','));
                             // console.log('\n\n')
                         }
+                        counter += 1;
                     });
                 }
             }
