43fa0a71d7cf6846a3e3df797ae910c790c8a9b7
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-09-20 14:11:10 -0700

    Now can be used as a command line or web app. Refactored the code. Added comments - Ready to publish for others to contirbute

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-var express = require('express');
-var app = express();
 var request = require('request').defaults({
     jar: true
 });
 var sleep = require('sleep');
+var D = false; // Toggle for printing debug strings on console
 
-var D = false;
+(function main() {
+    var commandLineArgs = process.argv;
+    // Should we start the app as a web server or a command line tool depends on the number of command line arguments when running this file
+    if (commandLineArgs.length === 6) {
+        // node index.js   PHX   HYD,BLR,BOM,DEL,MAA,GOI,IXE  2015-12-24T00:00:00.000,2015-12-25T00:00:00.000   2016-01-16T00:00:00.000,2016-01-17T00:00:00.000
+        var args = getRequestArgumentsFromCommandLine(commandLineArgs);
+        printCheapest(args.sources, args.destinations, args.startDates, args.endDates);
+    } else {
+        var express = require('express');
+        var app = express();
 
-app.get('/', function(req, res) {
+        var server = app.listen(3000, function() {
+            var port = server.address().port;
+            console.log('Flight search server listening at http://localhost:%s', port);
+        });
+
+        defineRoutes(app);
+    }
+})();
+
+function getRequestArgumentsFromCommandLine(args) {
+    // All possible source airpots you want to fly from. Airport acronym from Student Universe website
+    var sources = args[2].split(',').map(function(curr) {
+        return curr.trim();
+    });
+    // All possible destinations you can fly to. Airport acronyms from Student Universe website
+    var destinations = args[3].split(',').map(function(curr) {
+        return curr.trim();
+    });;
+    // All possible dates on which you want to start
+    var startDates = args[4].split(',').map(function(curr) {
+        return curr.trim();
+    });
+    // All possible dates you want to return back. Has to have at least one.
+    var endDates = args[5].split(',').map(function(curr) {
+        return curr.trim();
+    });
+
+    return {
+        sources: sources,
+        destinations: destinations,
+        startDates: startDates,
+        endDates: endDates
+    };
+}
+
+function printCheapest(sources, destinations, startDates, endDates) {
     var url = 'https://www.studentuniverse.com/wapi/flightsWapi/searchFlightsSpanned';
     var data = {
         "tripElements": [{
@@ -27,29 +70,27 @@ app.get('/', function(req, res) {
         "phoneInDiscountCode": null,
         "searchKey": null
     }
-    var table = {},
+    var comparison_table = {}, // price table of all possible combinations of sources, destination and dates
         counter = 1;
-    var sources = ['LAX', 'CHI', 'PHX', 'SFO', 'PDX', 'DFW', 'IAH', 'NYC', 'BOS'],
-        destinations = ['BOM', 'HYD', 'DEL', 'MAA', 'BLR'];
-    var startDates = ['2015-12-25T00:00:00.000', '2015-12-26T00:00:00.000'],
-        endDates = ['2016-01-17T00:00:00.000', '2016-01-18T00:00:00.000', '2016-01-19T00:00:00.000'];
 
-    // var sources = ['LAX', 'CHI', 'PHX'],
-    //     destinations = ['BOM', 'HYD', 'DEL', 'MAA', 'BLR'];
-    // var startDates = ['2015-12-16T00:00:00.000', '2015-12-17T00:00:00.000', ],
-    //     endDates = ['2016-01-03T00:00:00.000', '2016-01-04T00:00:00.000', '2016-01-05T00:00:00.000'];
+    // Total combinations of sources, destinations, start ends and return dates for logging
+    var total_combinations = sources.length * destinations.length * startDates.length * endDates.length;
+    var iprogress = total_combinations;
+    console.log('Total Combinations: ' + total_combinations);
+    var status_interval = total_combinations / 10; // for every 10% percent print status string
 
-    var total = sources.length * destinations.length * startDates.length * endDates.length;
-    var debug_count = total / 10; // for every 10% percent print debug string
-
+    // CSV Formatted output
     console.log('Sno,Cities,Start Date,End Date,Price,ToDuration,FroDuration,Airlines');
 
+    // Loop over each source, destination, start and end dates
     for (var isource = 0; isource < sources.length; isource++) {
         for (var idestination = 0; idestination < destinations.length; idestination++) {
-            table[sources[isource] + "-" + destinations[idestination]] = [];
+            comparison_table[sources[isource] + "-" + destinations[idestination]] = [];
             for (var isd = 0; isd < startDates.length; isd++) {
                 for (var ied = 0; ied < endDates.length; ied++) {
 
+                    // Required not to flood the Student Universe server. 
+                    // Can try setTimeout() also
                     sleep.usleep(500000); // 500 milli seconds
 
                     data.tripElements[0].dateTime = startDates[isd]; // set start date
@@ -60,11 +101,13 @@ app.get('/', function(req, res) {
                     data.tripElements[1].origin = destinations[idestination]; // set origin for return
                     data.tripElements[1].destination = sources[isource]; // set destination for return
 
-                    if (D && total % debug_count === 0) {
-                        console.log('Sent req: ', data.tripElements);
+                    // Print status of requests being sent at the intervals of `status_interval`
+                    if (D && iprogress % status_interval === 0) {
+                        console.log('Sent request: ', data.tripElements);
                     };
-                    total -= 1;
+                    iprogress -= 1;
 
+                    // HTTP request to StudentUniverse site
                     request({
                         method: 'POST',
                         uri: url,
@@ -72,26 +115,30 @@ app.get('/', function(req, res) {
                         json: true
                     }, function(error, response, body) {
                         if (error) {
+                            // Can be SOCKET Timeout, Server Hang Up etc.
                             console.log(counter, 'error', error);
                             //res.status(500).send("Error in POST call");
                         } else {
                             var cheapest_itinerary = {
-                                sd: null,
-                                ed: null,
-                                price: Infinity,
-                                toduration: null,
-                                froduration: null,
-                                airlines: null
+                                sd: null, // start date of the cheapest itinearary
+                                ed: null, // end date of the cheapest itinearary
+                                price: Infinity, // price of the cheapest itinearary
+                                toduration: null, // onward journey duration of the cheapest itinearary
+                                froduration: null, // return journey duration of the cheapest itinearary
+                                airlines: null // name of the airlines of the cheapest itinearary
                             };
                             var source, destination;
                             if (!body.itineraries) {
                                 if (D) {
+                                    // StudentUniverse couldnt find results for a source - destination - start date - end date combination 
                                     console.log(counter, 'Couldnt find itineraries for a combination');
                                 };
                             } else {
+                                // Find the cheapest itinerary from the list of ititneraries returned
                                 for (var i = 0; i < body.itineraries.length; i++) {
                                     var itinerary = body.itineraries[i];
                                     if (itinerary.total < cheapest_itinerary.price) {
+                                        // Set all the fields of the cheapest itinerary so far
                                         cheapest_itinerary.sd = itinerary.legs[0].departureTime;
                                         cheapest_itinerary.ed = itinerary.legs[itinerary.legs.length - 1].departureTime;
                                         cheapest_itinerary.price = itinerary.total;
@@ -105,19 +152,21 @@ app.get('/', function(req, res) {
                                         cheapest_itinerary.airlines = itinerary.carrierDescription;
                                     }
                                 }
-                                // console.log(source + '-' + destination);
-                                if (!table[source + '-' + destination]) {
-                                    table[source + '-' + destination] = [];
-                                };
-                                table[source + '-' + destination].push(cheapest_itinerary);
-                                // console.log('table', table);
-                                // console.log(source + '-' + destination, JSON.stringify(cheapest_itinerary));
+                                // Add the key of the form 'source-destination' if not present and initialize. Eg: LAX-BLR
+                                comparison_table[source + '-' + destination] =
+                                    comparison_table[source + '-' + destination] || [];
+                                comparison_table[source + '-' + destination].push(cheapest_itinerary);
+                                // print the output in CSV format on console
                                 console.log([counter, source + '-' + destination, cheapest_itinerary.sd, cheapest_itinerary.ed,
                                     cheapest_itinerary.price, cheapest_itinerary.toduration,
                                     cheapest_itinerary.froduration, cheapest_itinerary.airlines
                                 ].join(','));
+
+                                if (counter >= total_combinations) {
+                                    console.log('Done!')
+                                };
+
                             }
-                            // console.log('\n\n')
                         }
                         counter += 1;
                     });
@@ -125,11 +174,22 @@ app.get('/', function(req, res) {
             }
         }
     }
+}
 
-    res.send("Done")
-});
+function defineRoutes(app) {
+    app.get('/', function(req, res) {
+        // All possible source airpots you want to fly from. Airport acronym from Student Universe website
+        var sources = ['PHX'],
+            // All possible destinations you can fly to. Airport acronyms from Student Universe website
+            destinations = ['HYD', 'BLR', 'BOM'];
+        // All possible dates on which you want to start
+        var startDates = ["2015-12-24T00:00:00.000", "2015-12-25T00:00:00.000", "2015-12-26T00:00:00.000"],
+            // All possible dates you want to return back. Has to have at least one.
+            endDates = ["2016-01-16T00:00:00.000", "2016-01-17T00:00:00.000"];
 
-var server = app.listen(3000, function() {
-    var port = server.address().port;
-    console.log('Example app listening at http://localhost:%s', port);
-});
+        printCheapest(sources, destinations, startDates, endDates);
+
+        // send the response immediately and dont wait for all computations to complete
+        res.send("Triggered the requests. Check the console for results.");
+    });
+}
